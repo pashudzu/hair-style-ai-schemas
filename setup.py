2549ed83c36d062448abe14e8c485d1988916d7b
pashudzu <pavelcrosot@gmail.com>
2025-08-24 15:48:39 +0300

    rename schemas dir

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="hair_style_ai_schemas",
-    version="0.0.1",
+    name="hair-style-ai-schemas",
+    version="0.0.2",
     author="pashudzu",
     author_email="pashudzu@gmail.com",
     description="Library of schemas for project hair-style-ai",
     project_urls={"GitHub": "https://github.com/pashudzu/hair-style-ai-schemas"},
     packages=find_packages(),
     install_requires=["pydantic"],
+    license="MIT License",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
