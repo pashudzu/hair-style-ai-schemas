1a86c9d5b3be386abc481685c9e41840d6ef8953
pashudzu <pavelcrosot@gmail.com>
2025-09-07 16:31:16 +0300

    feat: add password and username validation for Latin letters and special symbols

--- hair_style_ai_schemas/auth.py
+++ hair_style_ai_schemas/auth.py
@@ -1,10 +1,29 @@
-from pydantic import BaseModel, EmailStr, Field
+import re
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 class RegistrationForm(BaseModel):
-    username: str = Field(..., min_length=6, max_length=25)
+    username: str = Field(...)
     email: EmailStr = Field(...)
-    password: str = Field(..., min_length=6, max_length=128)
+    password: str = Field(...)
+
+    @field_validator("username")
+    def username_complexity(cls, v) -> str:
+        if not re.fullmatch(r"[a-zA-Z0-9_-.]{6,25}", v):
+            raise ValueError("Username must contain only Latin letters, digits and allowed symbols(_-.). Minimum size is 6 and maximum is 25")
+        return v
+
+    @field_validator("password")
+    def password_complexity(cls, v) -> str:
+        if not re.fullmatch(r"[A-Za-z0-9!@#$%^&*()_+\-=\[\]{};:'\",.<>/?|`~]{6,64}", v):
+            raise ValueError("Password must contain only Latin letters, digits and allowed symbols(!@#$%^&* etc). Minimum size is 6 and maximum is 64.")
+        if not re.search(r"[a-z]", v):
+            raise ValueError("Password must contain at least one lowercase letter.")
+        if not re.search(r"[A-Z]", v):
+            raise ValueError("Password must contain at least one uppercase letter.")
+        if not re.search(r"[0-9]", v):
+            raise ValueError("Password must contain at least one digit.")
+        return v
 
 class LoginForm(BaseModel):
-    email_or_username: EmailStr = Field(...)
+    email_or_username: str = Field(..., pattern=r"^[a-zA-Z0-9_-. +-@]+$", max_length=254)
     password: str = Field(...)
