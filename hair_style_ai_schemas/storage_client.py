4e2eb28eeb9c7ba3ff63de766a342ab1791eeeed
pashudzu <pavelcrosot@gmail.com>
2025-12-24 15:57:17 +0300

    feat: add UploadFileForm

---
+++ hair_style_ai_schemas/storage_client.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, field_validator, Field
+
+class UploadFileForm(BaseModel):
+    file_name: str = Field(...)
+    bucket_name: str = Field(...)
+
+    @field_validator("file_name")
+    def file_name_ending(cls, v) -> str:
+        if not v.endswith((".jpg", ".jpeg", ".png")):
+            raise ValueError("File must be an image")
+        return v
